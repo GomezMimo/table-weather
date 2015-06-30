176743a6c7b1727d4aef635aa5b6982bc924f0c0
Juan Gomez <juan.camilo.mimo@gmail.com>
2015-06-30 11:54:47 -0500

    Initial Commit

---
+++ Mix/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mix
+//
+//  Created by Juan Gomez on 30/06/15.
+//  Copyright (c) 2015 Codes and Tags. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Mix/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mix
+//
+//  Created by Juan Gomez on 30/06/15.
+//  Copyright (c) 2015 Codes and Tags. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
