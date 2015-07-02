5b3f4a3fff95794087ea302d5acae25a902c0402
Juan Gomez <juan.camilo.mimo@gmail.com>
2015-07-01 22:09:29 -0500

    Combination between tables and NSURL-> Weather Expample

--- Mix/FirstViewController.swift
+++ Mix/FirstViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var cities = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet weak var table: UITableView!
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return cities.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = cities[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if NSUserDefaults.standardUserDefaults().objectForKey("cities") != nil{
+            cities = NSUserDefaults.standardUserDefaults().objectForKey("cities") as! [String]
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        table.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == UITableViewCellEditingStyle.Delete{
+            cities.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(cities, forKey: "cities")
+            table.reloadData()
+        }
     }
 
 
--- Mix/SecondViewController.swift
+++ Mix/SecondViewController.swift
@@ -8,18 +8,91 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    func showError(){
+        result.text = "The city hasn't been found"
+    }
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var cityField: UITextField!
+    @IBAction func searchButton(sender: AnyObject) {
+        var myString = cityField.text
+        var newString = myString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        if newString.isEmpty{
+            result.text = "Please fill the text field."
+        }else{
+            //Weather Part
+            var weather = ""
+            var urlError = false
+            var url = NSURL(string: "http://www.weather-forecast.com/locations/" + cityField.text.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+            if url != nil{
+                var task = NSURLSession.sharedSession().dataTaskWithURL(url!){
+                    (data, response, error) in
+                    if error == nil{
+                        var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+                        var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                        if urlContentArray.count > 1 {
+                            var weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+                            weather = weatherArray[0] as! String
+                            weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        }else{
+                            urlError = true
+                        }
+                        
+                    }else{
+                        urlError = true
+                    }
+                    dispatch_sync(dispatch_get_main_queue()){
+                        if urlError == false {
+                            self.result.text = "\(self.cityField.text.uppercaseString) \n\n" + weather
+                            //table part
+                            cities.append(self.cityField.text)
+                            self.cityField.text = ""
+                            println(cities)
+                            NSUserDefaults.standardUserDefaults().setObject(cities, forKey: "cities")
+                        }else{
+                            self.showError()
+                        }
+                    }
+                }
+                task.resume()
+            }else{
+                showError()
+            }
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.cityField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        cityField.text = ""
+        view.self.endEditing(true)
+        cityField.text = ""
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        var myString = cityField.text
+        var newString = myString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        if newString.isEmpty{
+            result.text = "Please fill the text field."
+        }else{
+            cities.append(cityField.text)
+            NSUserDefaults.standardUserDefaults().setObject(cities, forKey: "cities")
+            cityField.text = ""
+            result.text = ""
+        }
+        self.cityField.resignFirstResponder()
+        return true
+    }
 
 }
 
